3410c3aa200586ad9d827d38d3db88739946c2a6
tejeda22 <tejeda2219@gmail.com>
2020-01-02 10:19:26 -0600

    recursos google anilitics

--- resources/views/theme/lte/layout.blade.php
+++ resources/views/theme/lte/layout.blade.php
@@ -182,8 +182,16 @@
 						</script>
 						<script>
 							document.getElementById('vid').play();
-						</script>	
+						</script>
+						<!-- Global site tag (gtag.js) - Google Analytics -->
+						<script async src="https://www.googletagmanager.com/gtag/js?id=UA-155200639-1"></script>
+						<script>
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
 
+						gtag('config', 'UA-155200639-1');
+						</script>
 						
 				</body>
 			</html>
