7099eba205923e58e33bc2c4d24af508678402df
tejeda22 <tejeda2219@gmail.com>
2020-01-02 10:33:05 -0600

    modificacion layout

--- resources/views/theme/lte/layout.blade.php
+++ resources/views/theme/lte/layout.blade.php
@@ -8,6 +8,16 @@
 					<meta charset="utf-8">
 					<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 					<meta http-equiv="x-ua-compatible" content="ie=edge">
+
+					<!-- Global site tag (gtag.js) - Google Analytics -->
+					<script async src="https://www.googletagmanager.com/gtag/js?id=UA-155200639-1"></script>
+					<script>
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+
+					gtag('config', 'UA-155200639-1');
+					</script>
 									
 					<link rel="shortcut icon" href="../../favicon.ico" type="image/x-icon">
 					
@@ -183,16 +193,6 @@
 						<script>
 							document.getElementById('vid').play();
 						</script>
-						<!-- Global site tag (gtag.js) - Google Analytics -->
-						<script async src="https://www.googletagmanager.com/gtag/js?id=UA-155200639-1"></script>
-						<script>
-						window.dataLayer = window.dataLayer || [];
-						function gtag(){dataLayer.push(arguments);}
-						gtag('js', new Date());
-
-						gtag('config', 'UA-155200639-1');
-						</script>
-						
 				</body>
 			</html>
 		
