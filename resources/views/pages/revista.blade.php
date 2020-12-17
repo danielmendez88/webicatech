341e1af6984ad77d7f63c2d132fb50f516848663
tejeda22 <tejeda2219@gmail.com>
2020-12-17 14:40:36 -0600

    revista

--- resources/views/pages/revista.blade.php
+++ resources/views/pages/revista.blade.php
@@ -22,19 +22,23 @@
     </style>
 
 <div class="container g-pt-25">
+    <img src="revista_icatech/baner_revista.jpeg" class="img-responsive" width="100%" alig="center"> 
     <div class="masonry-grid row  row-eq-height ">
+        
+
+        
                     
         <div class="masonry-grid-item col-sm-6 col-md-6 col-lg-3 g-mb-30">
-            <article class="masonry-white-caption u-shadow-v32 u-shadow-v21--hover g-transition-0_3 h-100 g-rounded-top-7 g-rounded-bottom-7">
+            <br><article class="masonry-white-caption u-shadow-v32 u-shadow-v21--hover g-transition-0_3 h-100 g-rounded-top-7 g-rounded-bottom-7">
                 <a href="https://www.flipsnack.com/6AAB79CC5A8/primera_edicion_icatech.html" target="_blank" class="masonry-img-square">
-                    <img class="img-fluid" src="revista_icatech/revista_01.png" alt="Revista Icatech">
+                    <img class="img-fluid" src="revista_icatech/revista_1.png" alt="Revista Icatech">
                 </a>
 				<div class="g-pa-20 g-rounded-bottom-5 text-center">
 					<h2 class="h5 mb-3">
 					    <a class="u-link-v5  g-font-weight-600 g-color-accent--hover g-text-underline--hover" href="https://www.flipsnack.com/6AAB79CC5A8/primera_edicion_icatech.html" target="_blank">
 					    Primera Edición, Marzo 2020 Revista ICATECH <br>
                         </a>                        
-                        <br><a class="" href="revista_icatech/primera_edicion_icatech.pdf" target="_blank"><button class="button button1">Descargar</button></a>
+                        <br><a class="" href="revista_icatech/1_edicion_icatech.pdf" target="_blank"><button class="button button1">Descargar</button></a>
                         <a class="" href="https://www.flipsnack.com/6AAB79CC5A8/primera_edicion_icatech.html" target="_blank"> <button class="button button2">Leer</button> 
                         </a>
 
@@ -44,16 +48,16 @@
         </div>   
                     
         <div class="masonry-grid-item col-sm-6 col-md-6 col-lg-3 g-mb-30">
-            <article class="masonry-white-caption u-shadow-v32 u-shadow-v21--hover g-transition-0_3 h-100 g-rounded-top-5 g-rounded-bottom-5">
+            <br><article class="masonry-white-caption u-shadow-v32 u-shadow-v21--hover g-transition-0_3 h-100 g-rounded-top-5 g-rounded-bottom-5">
                 <a href="https://www.flipsnack.com/6AAB79CC5A8/segunda_edicion_icatech.html" target="_blank" class="masonry-img-square">
-                    <img class="img-fluid" src="revista_icatech/revista_02.png" alt="Trámites y servicios">
+                    <img class="img-fluid" src="revista_icatech/revista_2.png" alt="Trámites y servicios">
                 </a>
 				<div class="g-pa-20 g-rounded-bottom-5 text-center">
 					<h2 class="h5 mb-3">
 					    <a class="u-link-v5  g-font-weight-600 g-color-accent--hover g-text-underline--hover" href="https://www.flipsnack.com/6AAB79CC5A8/segunda_edicion_icatech.html" target="_blank">
 					    Segunda Edición, Agosto 2020 Revista ICATECH <br>
                         </a>
-                        <br><a class="" href="revista_icatech/segunda_edicion_icatech.pdf" target="_blank"><button class="button button1">Descargar</button></a>
+                        <br><a class="" href="revista_icatech/2_edicion_icatech.pdf" target="_blank"><button class="button button1">Descargar</button></a>
                         <a class="" href="https://www.flipsnack.com/6AAB79CC5A8/segunda_edicion_icatech.html" target="_blank"> <button class="button button2">Leer</button> 
                         </a>
 			        </h2>
@@ -62,16 +66,16 @@
         </div>   
                     
         <div class="masonry-grid-item col-sm-6 col-md-6 col-lg-3 g-mb-30">
-            <article class="masonry-white-caption u-shadow-v32 u-shadow-v21--hover g-transition-0_3 h-100 g-rounded-top-5 g-rounded-bottom-5">
+            <br><article class="masonry-white-caption u-shadow-v32 u-shadow-v21--hover g-transition-0_3 h-100 g-rounded-top-5 g-rounded-bottom-5">
                 <a href="https://www.flipsnack.com/6AAB79CC5A8/tercera_edicion_icatech.html" target="_blank" class="masonry-img-square">
-                    <img class="img-fluid" src="revista_icatech/revista_03.png" alt="Micrositio de Salud sobre Coronavirus">
+                    <img class="img-fluid" src="revista_icatech/revista_3.png" alt="Micrositio de Salud sobre Coronavirus">
                 </a>
 				<div class="g-pa-20 g-rounded-bottom-5 text-center">
 					<h2 class="h5 mb-3">
 					    <a class="u-link-v5  g-font-weight-600 g-color-accent--hover g-text-underline--hover" href="https://www.flipsnack.com/6AAB79CC5A8/tercera_edicion_icatech.html" target="_blank">
 					    Tercera Edición, Octubre 2020 Revista ICATECH <br>
                         </a>
-                        <br><a class="" href="revista_icatech/tercera_edicion_icatech.pdf" target="_blank"><button class="button button1">Descargar</button></a>
+                        <br><a class="" href="revista_icatech/3_edicion_icatech.pdf" target="_blank"><button class="button button1">Descargar</button></a>
                         <a class="" href="https://www.flipsnack.com/6AAB79CC5A8/tercera_edicion_icatech.html" target="_blank"> <button class="button button2">Leer</button> 
                         </a>
 			        </h2>
