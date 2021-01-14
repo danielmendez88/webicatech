ceb16c20673d6060df861f7a43cb26a14a668dae
tejeda22 <tejeda2219@gmail.com>
2021-01-14 09:48:06 -0600

    calendario

--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -126,6 +126,7 @@
                     <a href="https://www.chiapas.gob.mx/media/plan-desarrollo-2019/Plan-Estatal-de-Desarrollo-Chiapas-2019-2024.pdf" target="_new"><img src="Imagenes Pagina/banner-plan.png" width="600px"></a>&nbsp;&nbsp;&nbsp;&nbsp;
                     <a href="{{route('convocatoria.instructores_externos')}}" target="_blank"><img src="Imagenes Pagina/convocatoria_pagina.jpeg" width="500px"></a>&nbsp;&nbsp;&nbsp;&nbsp;
 					<a target="_blank" href="https://ijech.chiapas.gob.mx/parlamentojuvenil2021" target=""><img src="Imagenes Pagina/parla_juvenil.jpeg" width="470px"></a>&nbsp;&nbsp;&nbsp;&nbsp;
+					<a target="_blank" href="Imagenes Pagina/calendario_icatech.pdf" target=""><img src="Imagenes Pagina/calendario.jpeg" width="470px"></a>&nbsp;&nbsp;&nbsp;&nbsp;
 					<a href="/Integridad" target=""><img src="Imagenes Pagina/bannericatechintegridad.png" width="470px"></a>&nbsp;&nbsp;&nbsp;&nbsp;
 					<a href="/transparencia" target=""><img src="Imagenes Pagina/banner icatechtransparencia4.png" width="470px"></a>&nbsp;&nbsp;&nbsp;&nbsp;
 					<a href="/planarchivo" target=""><img src="Imagenes Pagina/programa_anual_icatech.png" width="470px"></a>&nbsp;&nbsp;&nbsp;&nbsp;
