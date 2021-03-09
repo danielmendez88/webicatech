bb0dd0c2fb101c9c291fd1d8820006fdc5b31f7d
tejeda22 <tejeda2219@gmail.com>
2021-03-09 08:19:09 -0600

    transparencia

--- resources/views/pages/transparencia.blade.php
+++ resources/views/pages/transparencia.blade.php
@@ -5,44 +5,48 @@
 @section('content')
 <!--llama contenido de la pagina -->
 
-	<div class="container g-pt-50">
-			<div class="masonry-grid row g-mb-0 row-eq-height ">
-				<div class="masonry-grid-sizer col-sm-12"></div>
-				<div class="masonry-grid-item col-sm-6 col-md-6 col-lg-12 g-mb-30">
+ <div class="container g-pt-50">
+	 <div class="masonry-grid row g-mb-0 row-eq-height ">
+	         <div class="masonry-grid-sizer col-sm-12"></div>
+				 <div class="masonry-grid-item col-sm-6 col-md-6 col-lg-12 g-mb-30">					
 					<img src="Imagenes Pagina/banner icatechtransparencia4.png" class="img-responsive" width="100%">
-				</div>
-				<div class="masonry-grid-item col-sm-12 col-md-12 col-lg-2 g-mb-30">
-					<a href="http://icatech.transparencia.chiapas.gob.mx" target="_new">
-						<img src="Imagenes Pagina/transparencia-icatech.png" class="center-img">
-					</a>
-				</div>
-				<div class="masonry-grid-item col-sm-12 col-md-12 col-lg-3 g-mb-30">
-					<a href="/cuenta-publica">
-						<img src="Imagenes Pagina/cuentapublica-icatech.png" class="center-img">
-					</a>
-				</div>
-				<div class="masonry-grid-item col-sm-12 col-md-12 col-lg-2 g-mb-30">
-					<a href="/sevac">
-						<img src="Imagenes Pagina/sevac-icatech.png" class="center-img">
-					</a>
-				</div>
-				<div class="masonry-grid-item col-sm-12 col-md-12 col-lg-3 g-mb-30">
-					<a href="/Integridad">
-						<img src="Imagenes Pagina/codigodecondcuta-icatech.png" class="center-img">
-					</a>
-				</div>
-				<div class="masonry-grid-item col-sm-12 col-md-12 col-lg-2 g-mb-30">
-					<a href="/licitaciones">
-						<img src="Imagenes Pagina/licitaciones-icatech.png" class="center-img">
-					</a>
-				</div>
-
-				
-			</div>
+				 </div>
+				     <div class="masonry-grid-item col-sm-12 col-md-12 col-lg-2 g-mb-30">
+					   <a href="http://icatech.transparencia.chiapas.gob.mx" target="_new">
+					   <img src="Imagenes Pagina/transparencia-icatech.png" class="center-img">
+					   </a>
+				     </div>
+				         <div class="masonry-grid-item col-sm-12 col-md-12 col-lg-3 g-mb-30">
+					       <a href="/cuenta-publica">
+						   <img src="Imagenes Pagina/cuentapublica-icatech.png" class="center-img">
+					       </a>
+				         </div>
+				         <div class="masonry-grid-item col-sm-12 col-md-12 col-lg-2 g-mb-30">
+					       <a href="/sevac">
+						   <img src="Imagenes Pagina/sevac-icatech.png" class="center-img">
+					       </a>
+				         </div>
+				         <div class="masonry-grid-item col-sm-12 col-md-12 col-lg-3 g-mb-30">
+					       <a href="/Integridad">
+						   <img src="Imagenes Pagina/codigodecondcuta-icatech.png" class="center-img">
+					       </a>
+				         </div>
+				         <div class="masonry-grid-item col-sm-12 col-md-12 col-lg-2 g-mb-30">
+					       <a href="/licitaciones">
+					       <img src="Imagenes Pagina/licitaciones-icatech.png" class="center-img">
+					       </a>
+				         </div>
+         </div>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			<a href="http://www.itaipchiapas.org.mx/actas-pleno/06-E-2021.pdf" title="Inai" target="_blank"><img src="Imagenes Pagina/informe.jpeg" class="img-responsive" width="20%" alig="center">
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			<a href="Imagenes Pagina/informe_2020.pdf" title="Inai" target="_blank"><img src="Imagenes Pagina/informe_labores2020.png" class="img-responsive" width="40%" alig="center">	
+						
 	</div>
 
 					<div class="container g-pt-50">
 					<div class="tab-pane active" id="#red">
+						
 						<hr>
 													
 							<!--<p>Vinculos a los diferentes portales de Transparencia Nacionales y Estatales</p>-->
