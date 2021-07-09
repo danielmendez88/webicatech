4836dd1ff97ff935b2e13a07f9843c2c8e7196a1
danielmendez88 <daniboymendez@gmail.com>
2021-07-09 12:03:04 -0500

    modificaciones finales en el documento de sitio web remasterizado

---
+++ app/Traits/bannerTrait.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Traits;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
+use App\Models\Banner;
+
+trait bannerTrait {
+    protected function getBanner($codigo) {
+       /**
+        * 
+        */
+        switch ($codigo) {
+            case 'revista':
+                # code...
+                $query = Banner::select('banner.id', 'banner.nombre', 'banner.activo', 'banner.path', 'banner.tipo_archivo', 'banner.slug', 'catalogo_banner.nombre_ubicacion', 'banner.href')
+                ->join('catalogo_banner', 'catalogo_banner.id', '=', 'banner.id_catbanner')
+                ->where([['banner.activo','=', true],['catalogo_banner.codigo','=', $codigo]])
+                ->take(3)
+                ->get();
+                break;
+            case 'videoteca':
+                # consulta para videoteca
+                $query = Banner::select('banner.id', 'banner.nombre', 'banner.activo', 'banner.path', 'banner.tipo_archivo', 'banner.slug', 'catalogo_banner.nombre_ubicacion', 'banner.href', 'banner.youtubeid')
+                ->join('catalogo_banner', 'catalogo_banner.id', '=', 'banner.id_catbanner')
+                ->where([['banner.activo','=', true],['catalogo_banner.codigo','=', $codigo]])
+                ->take(6)
+                ->get();
+                break;
+            default:
+                # query por defecto
+                $query =  Banner::select('banner.id', 'banner.nombre', 'banner.activo', 'banner.path', 'banner.tipo_archivo', 'banner.slug', 'catalogo_banner.nombre_ubicacion', 'banner.href')
+                ->join('catalogo_banner', 'catalogo_banner.id', '=', 'banner.id_catbanner')
+                ->where([['banner.activo','=', true],['catalogo_banner.codigo','=', $codigo]])
+                ->get();
+                break;
+        }
+        return $query;
+    }
+}
