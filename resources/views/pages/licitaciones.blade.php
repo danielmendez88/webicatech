c28bb8cb0eb3217cfcaa3e247e6de5c1e7739aa1
danielmendez88 <daniboymendez@gmail.com>
2022-03-18 15:21:03 -0600

    modificaciones y subida de documento acta de fallo

--- resources/views/pages/licitaciones.blade.php
+++ resources/views/pages/licitaciones.blade.php
@@ -14,44 +14,6 @@
         <div class="masonry-grid row  row-eq-height">
             <div class="masonry-grid-sizer col-sm-12"></div>
             <div class="masonry-grid-item col-sm-12 col-md-12 col-lg-12 g-mb-30">
-
-                    <p><h2><b>PROGRAMA ANUAL DE ADQUISICIONES:</b></h2></p>
-
-                    <div class="card rounded-0 g-brd-none">
-                      <div id="accordion-07-heading-04" class="u-accordion__header g-pa-0" role="tab">
-                        <h5 class="mb-0 g-font-size-default g-font-weight-700 g-pa-20a mb-0">
-                            <a class="collapsed d-flex g-color-main g-text-underline--none--hover" href="#accordion-07-body-04" data-toggle="collapse" data-parent="#accordion-07" aria-expanded="false" aria-controls="accordion-07-body-04">
-                              <span class="u-accordion__control-icon g-brd-right g-brd-gray-light-v4 g-color-primary text-center g-pa-20">
-                                <i class="fa fa-plus"></i>
-                                <i class="fa fa-minus"></i>
-                              </span>
-                              <span class="g-pa-20">
-                                2020 2021
-                              </span>
-                            </a>
-                        </h5>
-                      </div>
-                      <div id="accordion-07-body-04" class="collapse" role="tabpanel" aria-labelledby="accordion-07-heading-04" data-parent="#accordion-07">
-                        <div class="u-accordion__body g-bg-gray-light-v5 g-px-50 g-py-30">
-                            <ul class="list-unstyled">
-                              @foreach ($adquisiciones as $key => $value)
-                                <li class="g-brd-around g-brd-gray-light-v4 g-brd-left-3 g-brd-primary-left g-rounded-3 g-pa-20 g-mb-7">
-                                  <div class="d-flex justify-content-start">
-                                    <h5 class="g-font-weight-600 g-color-black">{{ $key }}</h5>
-                                  </div>
-                                  <div class="d-flex justify-content-between">
-                                      <ul class="u-list-inline"></ul>
-                                      <div class="align-self-center">
-                                        <a class="" href="{{ $value }}" target="_blank"><img src="Imagenes Pagina/2.png"alt=""></a>
-                                      </div>
-                                  </div>
-                                </li>
-                              @endforeach 
-                            </ul>
-                          </div>
-                      </div>
-                    </div>
-
                     <p><h2><b>LICITACIONES:</b></h2></p><br>
 
                     <p><b><h5>LICITACIONES FEDERALES</h5></b></p><br>
@@ -105,7 +67,7 @@
                               <i class="fa fa-minus"></i>
                             </span>
                             <span class="g-pa-20">
-                              Acta de Apertura de Propuestas Técnicas
+                              Acta de Fallo de Propuesta Técnica
                             </span>
                           </a>
                       </h5>
@@ -113,7 +75,7 @@
                     <div id="accordion-07-body-02" class="collapse" role="tabpanel" aria-labelledby="accordion-07-heading-02" data-parent="#accordion-07">
                       <div class="u-accordion__body g-bg-gray-light-v5 g-px-50 g-py-30">
                           <ul class="list-unstyled">
-                            @foreach ($acta_apertura_propuesta_tecnica as $key => $value)
+                            @foreach ($acta_fallo_propuesta_tecnica as $key => $value)
                               <li class="g-brd-around g-brd-gray-light-v4 g-brd-left-3 g-brd-primary-left g-rounded-3 g-pa-20 g-mb-7">
                                 <div class="d-flex justify-content-start">
                                   <h5 class="g-font-weight-600 g-color-black">{{ $key }}</h5>
